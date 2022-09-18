2fa51f4e972485314bef7365e686df03142b929f
Daniil Pylenok <danila.pwork@mail.ru>
2022-09-18 14:51:02 +0300

    mini bug

--- src/Game.js
+++ src/Game.js
@@ -27,56 +27,39 @@ class Game {
   createEnemy() {
     setInterval(() => {
       this.enemy.push(new Enemy());
-    }, 1400);
+    }, 1000);
   }
 
   regenerateTrack() {
+    this.track = (new Array(5)).fill('').map(() => new Array(30).fill(' '));
+    this.track[this.hero.row][this.hero.position] = this.hero.skin;
+    if (this.boomerang.counter === 'right' || this.boomerang.counter === 'left') {
+      this.track[this.boomerang.row][this.boomerang.position] = this.boomerang.skin;
+    }
+    if (this.boomerang.position - this.hero.position > 8) {
+      this.boomerang.counter = 'left';
+    }
+    if (this.boomerang.position === this.hero.position && this.hero.row === this.boomerang.row) {
+      this.boomerang.counter = 'behind';
+    }
     this.enemy.forEach((el, index) => {
       el.moveLeft();
-
-      // очистка незадействованных полей
-      this.track[el.row][el.position + 1] = ' ';
-      this.track[this.hero.row][this.hero.position + 1] = ' ';
-      this.track[this.hero.row][this.hero.position - 1] = ' ';
-      this.track[this.boomerang.row][this.boomerang.position + 1] = ' ';
-      this.track[this.boomerang.row][this.boomerang.position - 1] = ' ';
-      if (this.hero.row > 0) {
-        this.track[this.hero.row - 1][this.hero.position] = ' ';
-      }
-      if (this.hero.row < 4) {
-        this.track[this.hero.row + 1][this.hero.position] = ' ';
-      }
       this.track[el.row][el.position] = el.skin;
-      this.track[this.hero.row][this.hero.position] = this.hero.skin;
-      if (this.boomerang.counter === 'right' || this.boomerang.counter === 'left') {
-        this.track[this.boomerang.row][this.boomerang.position] = this.boomerang.skin;
-      }
       // проверка позиция бумеранга и врага
       if (this.boomerang.row === el.row) {
         if (el.position === this.boomerang.position + 1
           || el.position === this.boomerang.position - 1
           || el.position === this.boomerang.position) {
-          this.track[el.row][el.position] = ' ';
           this.enemy.splice(1, index);
           this.score += 1;
           this.boomerang.counter = 'left';
         }
       }
-
       // проверка позиции героя и врага
       if (el.row === this.hero.row) {
         if (el.position === this.hero.position) {
           this.hero.die(this.score);
         }
-      }
-      // возвращение бумеранга
-      if (this.boomerang.position - this.hero.position > 8) {
-        this.boomerang.counter = 'left';
-      }
-
-      // бумеранг за спиной
-      if (this.boomerang.position === this.hero.position && this.hero.row === this.boomerang.row) {
-        this.boomerang.counter = 'behind';
       }
     });
     this.boomerang.fly();
